319ab0495e5f4f417ff6b5353a9f4d60a776da74
KrukRecon <adam.kruk00@gmail.com>
2022-02-08 22:07:19 +0100

    ControllingProgramFlow: module 3

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -7,7 +8,59 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            GeneratingWinnerEmails();
             GenerateTasksReport();
+            GenerateCommentsReport();
+        }
+
+        public static void GeneratingWinnerEmails()
+        {
+            var selectedEmails = new List<string>();
+            int counter = 0;
+
+            Console.WriteLine(Environment.NewLine + "Selected Winners Output: ");
+            while (selectedEmails.Count < 2 && counter < Q1Results.Responses.Count)
+            {
+                var currentItem = Q1Results.Responses[counter];
+
+                if (currentItem.FavoriteProduct == "Cappucino")
+                {
+                    selectedEmails.Add(currentItem.EmailAddress);
+                    Console.WriteLine(currentItem.EmailAddress);
+                }
+
+                counter++;
+            }
+
+            File.WriteAllLines("WinnersReport.csv", selectedEmails);
+        }
+
+        public static void GenerateCommentsReport()
+        {
+            var comments = new List<string>();
+
+            Console.WriteLine(Environment.NewLine + "Comments output: ");
+            for (int i = 0; i < Q1Results.Responses.Count; i++)
+            {
+                var currentResponse = Q1Results.Responses[i];
+
+                if (currentResponse.WouldRecommend < 7)
+                {
+                    Console.WriteLine(currentResponse.Comments);
+                    comments.Add(currentResponse.Comments);
+                }
+            }
+
+            foreach (var response in Q1Results.Responses)
+            {
+                if (response.AreaToImprove == Q1Results.AreaToImprove)
+                {
+                    Console.WriteLine(response.Comments);
+                    comments.Add(response.Comments);
+                }
+            }
+
+            File.WriteAllLines("CommentsReport.csv", comments);
         }
 
         public static void GenerateTasksReport()
@@ -61,6 +114,14 @@ namespace WiredBrainCoffeeSurveys.Reports
                     tasks.Add("Investigate individual comments for ideas");
                     break;
             }
+
+            Console.WriteLine(Environment.NewLine + "Tasks output: ");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            File.WriteAllLines("TasksReport.csv", tasks);
         }
     }
 }
--- WiredBrainCoffeeSurveys.Reports/Q1Results.cs
+++ WiredBrainCoffeeSurveys.Reports/Q1Results.cs
@@ -9,7 +9,7 @@ namespace WiredBrainCoffeeSurveys.Reports
         // Aggregate ratings
         public static double ServiceScore { get; set; } = 8.0;
 
-        public static double CoffeeScore { get; set; } = 8.5;
+        public static double CoffeeScore { get; set; } = 4.0;
 
         public static double PriceScore { get; set; } = 6.0;
 
@@ -29,5 +29,114 @@ namespace WiredBrainCoffeeSurveys.Reports
         public static double NumberResponded { get; set; } = 325;
 
         public static double NumberRewardsMembers { get; set; } = 130;
+
+        // Individual survey responses
+        public static List<SurveyResponse> Responses = new List<SurveyResponse>()
+        {
+            new SurveyResponse()
+            {
+                EmailAddress = "test1@sample.com",
+                ServiceScore = 8.5,
+                CoffeeScore = 8.0,
+                PriceScore = 7.0,
+                FoodScore = 9.0,
+                WouldRecommend = 8.0,
+                FavoriteProduct = "Latte",
+                LeastFavoriteProduct = "Fruit",
+                AreaToImprove = "MobileApp",
+                Comments = "Coffee and service are great!",
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test2@sample.com",
+                ServiceScore = 7.5,
+                CoffeeScore = 10.0,
+                PriceScore = 7.0,
+                FoodScore = 6.0,
+                WouldRecommend = 8.0,
+                FavoriteProduct = "Cappucino",
+                LeastFavoriteProduct = "Fruit",
+                AreaToImprove = "MobileApp",
+                Comments = "The mobile app looks bad on Android devices",
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test3@sample.com",
+                ServiceScore = 8.5,
+                CoffeeScore = 8.0,
+                PriceScore = 6.5,
+                FoodScore = 7.0,
+                WouldRecommend = 8.0,
+                FavoriteProduct = "Americano",
+                LeastFavoriteProduct = "Fruit",
+                AreaToImprove = "Cleanliness",
+                Comments = "Overall i had a great experience, I like the store design",
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test4@sample.com",
+                ServiceScore = 8.5,
+                CoffeeScore = 9.0,
+                PriceScore = 7.5,
+                FoodScore = 5.0,
+                WouldRecommend = 4.0,
+                FavoriteProduct = "Cappucino",
+                LeastFavoriteProduct = "Fruit",
+                AreaToImprove = "Pricing",
+                Comments = "The pickup area shoul be by the door. Also Your prices are much too high",
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test5@sample.com",
+                ServiceScore = 8.5,
+                CoffeeScore = 8.0,
+                PriceScore = 6.5,
+                FoodScore = 7.0,
+                WouldRecommend = 6.0,
+                FavoriteProduct = "Iced Coffee",
+                LeastFavoriteProduct = "Fruit",
+                AreaToImprove = "Other",
+                Comments = "I like the store but your coffee is too expensive",
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test6@sample.com",
+                ServiceScore = 8.5,
+                CoffeeScore = 8.0,
+                PriceScore = 6.5,
+                FoodScore = 7.0,
+                WouldRecommend = 7.5,
+                FavoriteProduct = "Green tea macha",
+                LeastFavoriteProduct = "Fruit",
+                AreaToImprove = "MobileApp",
+                Comments = "Your staff are very friendly but your mobile app is too slow",
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test7@sample.com",
+                ServiceScore = 8.5,
+                CoffeeScore = 8.0,
+                PriceScore = 6.5,
+                FoodScore = 7.0,
+                WouldRecommend = 7.5,
+                FavoriteProduct = "Green tea macha",
+                LeastFavoriteProduct = "Fruit",
+                AreaToImprove = "MobileApp",
+                Comments = "The mobile app doesn't work on tablets",
+            },
+            new SurveyResponse()
+            {
+                EmailAddress = "test8@sample.com",
+                ServiceScore = 8.5,
+                CoffeeScore = 8.0,
+                PriceScore = 6.5,
+                FoodScore = 7.0,
+                WouldRecommend = 6.5,
+                FavoriteProduct = "Green tea macha",
+                LeastFavoriteProduct = "Fruit",
+                AreaToImprove = "MobileApp",
+                Comments = "So close to recommending but your prices are so high",
+            },
+        };
     }
 }
